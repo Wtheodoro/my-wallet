83f62e3552b68a67c67065db4cbe29a648788b1c
Walison Teodoro <theodoro.walison@gmail.com>
2021-04-13 00:54:21 -0300

    Side bar

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
-
-import { Container } from './styles';
+import { Container, Header, LogoImg, Title, Menu, MenuItemLink } from './styles';
+import { MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp } from 'react-icons/md'
+import logoImg from '../../assets/logo.svg'
 
 const Aside: React.FC = () => {
   return (
     <Container>
-      <h2>Hi from Aside</h2>
+      <Header>
+        <LogoImg src={logoImg} alt="Logo My Wallet" />
+        <Title>My Wallet</Title>
+      </Header>
+
+      <Menu>
+        <MenuItemLink href="#">
+          <MdDashboard />
+          DashBoard
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdArrowUpward />
+          Incomes
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdArrowDownward />
+          Expenses
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdExitToApp />
+          Sign Out
+        </MenuItemLink>
+      </Menu>
     </Container>
   )
 }
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -4,5 +4,47 @@ export const Container = styled.div`
     grid-area: Aside;
     
     background-color: ${props => props.theme.colors.secondary};
-    color: ${props => props.theme.colors.font}
+    padding-left: 1.2rem;
+    border-right: 1px solid ${props => props.theme.colors.gray};
+`;
+
+export const Header = styled.header`
+    height: 70px;
+    display: flex;
+    align-items: center;
+`;
+
+export const LogoImg = styled.img`
+    height: 3rem;
+    width: 3rem;
+`;
+
+export const Title = styled.h3`
+    color : ${props => props.theme.colors.font};
+    margin-left: 0.6rem;
+`;
+
+export const Menu = styled.nav`
+    display: flex;
+    margin-top: 3.5rem;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const MenuItemLink = styled.a`
+    color : ${props => props.theme.colors.info};
+    text-decoration: none;
+    margin: 0.5rem 0;
+    display: flex;
+    align-items: center;
+    transition: opacity .3s;
+
+    &:hover {
+        opacity: .7;
+    }
+
+    > svg {
+        font-size: 1.2rem;
+        margin-right: 0.5rem;
+    }
 `;
