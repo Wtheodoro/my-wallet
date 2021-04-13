301734b1115b04dab0d11a3bbd6de25862123a7c
Walison Teodoro <theodoro.walison@gmail.com>
2021-04-13 18:03:14 -0300

    scroll bar

--- src/components/contentSet/Content/styles.ts
+++ src/components/contentSet/Content/styles.ts
@@ -5,4 +5,22 @@ export const Container = styled.div`
     background-color: ${props => props.theme.colors.primary};
     color: ${props => props.theme.colors.font};
     padding: 1.6rem;
+
+    /* total viewl heig minus main header height */
+    height: calc(100vh - 70px);
+    overflow-y: scroll;
+
+    ::-webkit-scrollbar {
+        width: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: ${props => props.theme.colors.secondary};
+        border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background-color: ${props => props.theme.colors.tertiary};
+        border-radius: 10px;
+    }
 `;
